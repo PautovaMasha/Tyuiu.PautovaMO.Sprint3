c298b91bea03761c8468f11712be4518b71e828b
PautovaMO <Pautova812007@mail.ru>
2025-10-23 22:22:19 +0500

    Task2

---
+++ Tyuiu.PautovaMO.Sprint3.Task2.V6/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.PautovaMO.Sprint3.Task2.V6.Lib;
+
+namespace Tyuiu.PautovaMO.Sprint3.Task2.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил : Паутова М.О. | ИБКСб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do while                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Паутова Мария Олеговна | ИБКСб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do while, которая вычисляет сумму     *");
+            Console.WriteLine("* ряда по формуле, при a = 0.25                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine(" Переменная A " + value);
+            Console.WriteLine(" Начало " + startValue);
+            Console.WriteLine(" Конец " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
+
